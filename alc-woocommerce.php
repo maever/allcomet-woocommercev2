35f59117997f89d44d5ee746709b1b1a60e3e231
maever <maever@gmail.com>
2025-10-15 15:15:05 +0200

    Ensure gateway class loads after WooCommerce

--- alc-woocommerce.php
+++ alc-woocommerce.php
@@ -49,6 +49,9 @@ add_action('init', 'allcomet_gateway_load_textdomain');
 function allcomet_gateway_plugins_loaded(): void
 {
     if (! class_exists('WC_Payment_Gateway')) {
+        // WooCommerce defers loading the base gateway class until it finishes bootstrapping, so queue up a retry.
+        add_action('woocommerce_loaded', 'allcomet_gateway_plugins_loaded');
+
         return;
     }
 
